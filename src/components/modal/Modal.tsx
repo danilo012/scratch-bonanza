bb75ddc61425899f2906e850f7b2b1820079360c
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-01-29 05:42:04 +0800

    Update Modal.tsx

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -34,9 +34,13 @@ const Modal = () => {
             If you are on your computer, click on one of the areas and move your
             mouse around.
           </div>
+          <div className="modal-text">
+            Currency Factor for SCRATCH TO EARN. 
+We pay a  COINS for qualified “screen outs” (user gets redirected to a game , starts it but does not qualify to some reason) and game ratings (user gives his rating after a successful game complete). To know which reward we have to display the user in USD currency. the value based on the ration of 1 USD. Example: If user earned Coins and 1000 Coins have a value of 1 USD on our game settings , default settings would set the bonus factor to 1 USD = 1000 (Coins), so that the user gets 10 Coins for a 0.01 USD.
+            
+          </div>
           <div className="modal-text">Scratch your way to great prizes!</div>
-          <div className="modal-button" onClick={handleClear}>
-            Clear Data
+          
           </div>
         </div>
       </div>
